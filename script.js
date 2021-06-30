0d62ddbe4085cf7e44b840b1634bb5c9e32c2eb5
Deepam <asnani.deepam@gmail.com>
2021-06-30 16:36:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var nasaImages = $("#nasa-images");
+var solInput = $("#sol");
+var pageInput = $("#page")
+
+$("form button").click(function (e) {
+    e.preventDefault();
+    
+    let sol = solInput.val();
+    let page = pageInput.val();
+    console.log(sol, page);
+    
+    if( sol === "" || page === "") {
+        alert("Please fill the field");
+        return;
+    }
+
+    let url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + sol + "&page=" + page + "&api_key=NBlCLhD21Eud5RxMy1TjZoeJedDa1c1qbsnLMIG2";
+
+    $.get(url,function(data){
+        let photos = data.photos;
+       
+        $("#nasa-images img").remove();
+        for(let photo of photos){
+            nasaImages.append('<img src ="'+photo.img_src+'">');
+        }
+
+    });
+
+
+});
